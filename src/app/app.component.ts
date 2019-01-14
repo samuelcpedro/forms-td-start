528dc1f4f85caf3ba78bb58ae4de77af85198aa9
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-14 15:46:15 -0100

    TD: Submitting and Using the Form
    TD: Submitting and Using the Form
    Section 15, Lecture 177

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,24 @@ export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  // onSubmit(form: HTMLFormElement) {
+  //   console.log('Submitted!', form);
+  // }
+
+  // will return this
+  // <form _ngcontent-c0="" novalidate="" class="ng-valid ng-dirty ng-touched">...</form>
+
+  onSubmit(form: NgForm) {
+    console.log('Submitted!', form);
+  }
+  // Will return this
+  // NgForm {submitted: true, _directives: Array(3), ngSubmit: EventEmitter, form: FormGroup}
+
+  // where we can get the values of the form
+  //   value: Object
+  //     email: "mail value"
+  //     secret: "teacher"
+  //     username: "name value"
+  //
 }
