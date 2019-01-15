fea72520b83505e0730cecee52dcbc4e8aa34ec2
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-15 10:47:26 -0100

    TD: Using Form Data
    TD: Using Form Data
    Section 15, Lecture 189

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,14 @@ export class AppComponent {
   // defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -57,7 +65,13 @@ export class AppComponent {
   //
 
   onSubmit() {
-    console.log('Submitted!', this.signupForm);
+    // console.log('Submitted!', this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 
 }
